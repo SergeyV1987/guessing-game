1c9fd35279fc42a28412bc8221fb2efc924513fd
Admin <kurchinskayavera@gmail.com>
2019-10-01 22:11:07 +0300

    done

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,26 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        this.leftRange = 0;
+        this.rightRange = 0;
+        this.previousAssumption = 0;
+    }
 
     setRange(min, max) {
-
+        this.leftRange = min;
+        this.rightRange = max;
     }
 
     guess() {
-
+        this.previousAssumption = Math.ceil((this.rightRange - this.leftRange) / 2) + this.leftRange;
+        return this.previousAssumption;
     }
 
     lower() {
-
+         this.rightRange = this.previousAssumption;
     }
 
     greater() {
-
+        this.leftRange = this.previousAssumption;
     }
 }
 
